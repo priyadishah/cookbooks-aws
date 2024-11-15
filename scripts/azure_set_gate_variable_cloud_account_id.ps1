5bb0b453734e5279f8fc363b6d64b3997976c391
Rob Goodridge <goodridge.rob@gmail.com>
2024-11-15 16:19:14 +1100

    163681 Report error when artefact does not exist

--- scripts/azure_set_gate_variable_cloud_account_id.ps1
+++ scripts/azure_set_gate_variable_cloud_account_id.ps1
@@ -45,5 +45,5 @@ if (Test-Path $path) {
     Write-Host "##vso[task.setvariable variable=Sku;isOutput=true]$sku"
     Write-host "The value of Variable IsEnabled is updated to True and output variable ImageUrl to $uri"
 } else {
-    Write-Host "Artifact path $path does NOT exist for $Version"
+    throw "Artifact path $path does NOT exist for $Version"
 }
